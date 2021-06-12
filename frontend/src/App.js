475052e9505320421a9706e4f3bfa02341f19435
Pamela Brahollari <ebirdyxx@gmail.com>
2021-06-12 11:20:21 -0400

    Added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import './App.css';
 import ProductPage from "./components/ProductPage";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
-      <ProductPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path='/'>
+            <ProductPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
